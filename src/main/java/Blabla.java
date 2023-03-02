fa48054a18081591a2703eceef7a8a8247cc11b2
admin <alperen87@hotmail.com>
2023-03-02 10:35:55 +0100

    zwei neue Klassen erstellt

--- src/main/java/Blabla.java
+++ src/main/java/Blabla.java
@@ -1,2 +1,11 @@
 public class Blabla {
+
+    static int i = 5;
+
+    public static int gibInt(){
+        return i;
+    }
+    public static String macheBlub(){
+        return "Blub";
+    }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Blabla.gibInt());
+        System.out.println(Blabla.macheBlub());;
+    }
+}
